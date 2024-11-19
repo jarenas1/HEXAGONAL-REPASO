3a6b6b98be977e77701b8a2aaf3d4640ffed1268
Juan José Arenas G. <juanjoarenas1218@gmail.com>
2024-11-19 17:20:46 -0500

    initial commit

---
+++ src/main/java/com/todoHexagonal/hexagonalRepaso/HexagonalRepasoApplication.java
@@ -0,0 +1,13 @@
+package com.todoHexagonal.hexagonalRepaso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalRepasoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexagonalRepasoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/todoHexagonal/hexagonalRepaso/HexagonalRepasoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todoHexagonal.hexagonalRepaso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalRepasoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
